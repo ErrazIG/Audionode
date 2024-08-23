4da7336ccac785d98deb425e3f4dd8ab7ee5e9bc
ErrazIG <frederic-vml@hotmail.fr>
2024-08-23 22:54:41 +0200

    ajout navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import MusicPlayer from "./components/musicPlayer/MusicPlayer.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
 import Searchbar from "./components/searchbar/Searchbar.jsx";
 
 function App() {
   return (
     <>
       <div className="container">
-        <div className="logo"></div>
+        <Navbar />
         <div className="content">
           <Searchbar />
         </div>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import style from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={style.navbar}>
+      <h1 className={style.logo}>Audionode</h1>
+      <ul>
+        <li>
+          <a href="#">///</a>
+        </li>
+        <li>
+          <a href="#">///</a>
+        </li>
+        <li>
+          <a href="#">///</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
