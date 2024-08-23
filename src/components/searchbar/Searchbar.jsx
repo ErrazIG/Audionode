7be675fbfc1a59eca4115cf09af72b2feda6e70b
ErrazIG <frederic-vml@hotmail.fr>
2024-08-23 22:55:01 +0200

    chnage input text with txt area

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,15 +1,48 @@
-import { CiSearch } from "react-icons/ci";
+import { useState } from "react";
+import { AiOutlineEnter } from "react-icons/ai";
 import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
+import { IoIosSearch } from "react-icons/io";
 import style from "./SearchBar.module.css";
+
 const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Search Query:", searchQuery);
+    setSearchQuery("");
+  };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit(event);
+    }
+  };
+
   return (
-    <div className={style.searchbar}>
-      <input className={style.input} type="text" placeholder="Search a song" />
+    <form onSubmit={handleSubmit} className={style.searchbar}>
+      <textarea
+        className={style.input}
+        placeholder="Search a song"
+        value={searchQuery}
+        onChange={(event) => setSearchQuery(event.target.value)}
+        onKeyPress={handleKeyPress}
+      />
       <div className={style.button}>
-        <CiSearch className={style.searchIcon} />
-        <GiPerspectiveDiceSixFacesRandom className={style.searchIcon} />
+        <div className={style.keys}>
+          <IoIosSearch className={style.searchIcon} onClick={handleSubmit} />
+          <p className={style.keysIcon}>
+            <AiOutlineEnter className={style.key} />
+          </p>
+        </div>
+        <div className={style.keys}>
+          <GiPerspectiveDiceSixFacesRandom className={style.searchIcon} />
+          <p className={style.keysIcon}>
+            <span className={`${style.key} ${style.rKey}`}>R</span>
+          </p>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
+
 export default SearchBar;
